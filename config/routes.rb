7a0c2c90f76bfe68f72fb13e839c5374f9418da1
andrewmcodes <am5648@uncw.edu>
2018-03-28 13:28:42 -0400

    projects scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :projects
   resources :teams
   as :user do
     put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
