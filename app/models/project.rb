f5010ab15feff1d2e8104c275c3ab2927451ea79
andrewmcodes <am5648@uncw.edu>
2018-04-01 08:28:09 -0400

    fix bug and cops

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,10 @@
-class Project < ApplicationRecord
-	belongs_to :team
-	belongs_to :user
+# frozen_string_literal: true
 
-	accepts_nested_attributes_for :team
-	include PublicActivity::Model
-	tracked owner: Proc.new { |controller, model| controller.current_user }
+class Project < ApplicationRecord
+  belongs_to :team
+  belongs_to :user
+
+  accepts_nested_attributes_for :team
+  include PublicActivity::Model
+  tracked owner: proc { |controller, _model| controller.current_user }
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,10 @@
-class Team < ApplicationRecord
-	has_many :projects, dependent: :destroy
-	has_many :users
+# frozen_string_literal: true
 
-	accepts_nested_attributes_for :users, allow_destroy: true
-	include PublicActivity::Model
-	tracked owner: Proc.new { |controller, model| controller.current_user }
+class Team < ApplicationRecord
+  has_many :projects, dependent: :destroy
+  has_many :users
+
+  accepts_nested_attributes_for :users, allow_destroy: true
+  include PublicActivity::Model
+  tracked owner: proc { |controller, _model| controller.current_user }
 end
